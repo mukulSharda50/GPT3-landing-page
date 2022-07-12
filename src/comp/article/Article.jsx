6c9f96107c5e34a40440b4315a5232903ae342d5
Mukul Sharda <mukulsharda5@gmail.com>
2022-07-12 13:19:33 +0530

    Implemented Blogs section

--- src/comp/article/Article.jsx
+++ src/comp/article/Article.jsx
@@ -1,6 +1,18 @@
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
-};
+const Article = ({ img, date, desc}) => (
+  <div className="gpt3__article-container">
+    <div className="gpt3__article-img">
+      <img src={img} alt={"blog header"}/>
+    </div>
+    <div className="gpt3__article-content">
+      <div className="gpt3__article-heading">
+        <p>{ date }</p>
+        <h3>{ desc }</h3>
+      </div>
+      <p>Read Full Article</p>
+    </div>
+  </div>
+);
+
 export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,11 +1,27 @@
 import "./blog.css";
+import { Article } from "../../comp";
+import blog01 from '../../assets/blog01.png';
+import blog02 from '../../assets/blog02.png';
+import blog03 from '../../assets/blog03.png';
+import blog04 from '../../assets/blog04.png';
+import blog05 from '../../assets/blog05.png';
 
 const Blog = () => {
   return (
-    <>
-      hello hello hello
-      <div></div>
-    </>
+    <div className="gpt3__blog-container" id="casestudy">
+      <h2>A lot is happening, We are blogging about it.</h2>
+      <div className="gpt3__blog-blogposts">
+        <div className="gpt3__blog-blogposts-left">
+          <Article img={blog01} date={"Sep 26, 2021"} desc={"GPT-3 and Open  AI is the future. Let us exlore how it is?"} />
+        </div>
+        <div className="gpt3__blog-blogposts-right">
+          <Article img={blog02} date={"Sep 26, 2021"} desc={"GPT-3 and Open  AI is the future. Let us exlore how it is?"} />
+          <Article img={blog03} date={"Sep 26, 2021"} desc={"GPT-3 and Open  AI is the future. Let us exlore how it is?"} />
+          <Article img={blog04} date={"Sep 26, 2021"} desc={"GPT-3 and Open  AI is the future. Let us exlore how it is?"} />
+          <Article img={blog05} date={"Sep 26, 2021"} desc={"GPT-3 and Open  AI is the future. Let us exlore how it is?"} />
+        </div>
+      </div>
+    </div>
   );
 };
 export default Blog;
